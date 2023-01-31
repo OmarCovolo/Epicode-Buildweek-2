942ed088c5f29cab4661cb5a35a2664f1017ab2f
Eleonora Mattera <m.eleonora98@live.it>
2023-01-31 16:51:25 +0100

    html css

--- amici.js
+++ amici.js
@@ -1,3 +1,15 @@
 const nascondi = (event) => {
   event.target.parentElement.parentElement.parentElement.parentElement.remove();
 };
+
+var aperto = false;
+
+function toggle() {
+  if (!aperto) {
+    document.getElementById("roww").style.visibility = "visible";
+    aperto = true;
+  } else {
+    aperto = false;
+    document.getElementById("roww").style.visibility = "hidden";
+  }
+}
