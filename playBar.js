9e1a3d0c39c1e5c1dfc4b8e9df1427091762e4a8
MicheleCe <michele.cerqua@hotmail.com>
2023-02-02 14:46:55 +0100

    .

--- playBar.js
+++ playBar.js
@@ -9,6 +9,8 @@ function handleSearchOnClick (){
   //console.log(queryToSearch);
   songSearch(queryToSearch)
 }
+let index = API_STRIVES_SCHOOL + '?q=' + "*"
+window.onload = () =>{songSearch(index)}
 
 const songSearch = (input) => {
   fetch(input)
@@ -22,6 +24,15 @@ const songSearch = (input) => {
   .then((jsonPromise) => {
       let data = jsonPromise.data;
       console.log(data);
+      let newsReference = document.querySelector('.containerJ')
+      newsReference.innerHTML += albumNews(
+        data[0].album.cover_big,
+        data[0].artist.name,
+        data[0].album.title,
+        data[0].preview,
+        data[0].album.id,
+        data[0].title_short,
+      )
       data.forEach((element, i) => {
           let listReference = document.querySelector('.containerJs1')
           listReference.innerHTML += containerAlbum(
@@ -52,6 +63,27 @@ function containerAlbum(songTitle, albumCover, artistName, albumtitle, artistPic
 </div>
   `
 }
+
+function albumNews(albumCover, artistName, albumtitle, track, artistid, albumId, i, title) {
+    console.log("ddd", track );
+    return `
+  <div class="newsCont col-12 col-md-3 text-light d-flex m-3">
+      <img src="${albumCover}" onclick='bringToAlbumPage("${albumtitle}","${albumId}")' alt="Card_image"/>
+      <div class="d-flex flex-column ms-3">
+        <h6>Album</h6>
+        <h1>${albumtitle}</h1>
+        <p onclick='bringToArtistPage("${artistName}","${artistid}")'>${artistName}</p>
+        <p>Ascolta il nuovo singolo di ${artistName}</p>
+        <div class="d-flex">
+          <button class="bi play text-white mx-2 fs-5 rounded-5 bg-dark text-center border-1">Salva</button>
+          <button onclick='playMusic("${track}", "${title}", "${artistName}")' class="bi play text-white mx-2 fs-4 rounded-5 bg-success text-center ">Play</button>
+        </div>
+      </div>
+  </div>
+  `
+}
+
+
 
 function bringToArtistPage(artist, id) {
   let infoArr = [artist, id]
